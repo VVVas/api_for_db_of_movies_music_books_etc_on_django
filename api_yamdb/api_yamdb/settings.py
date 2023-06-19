d9610a8d110b0ca739e4bfcddba863374eb4dd07
VVVas <v@vvvas.ru>
2023-06-20 01:12:01 +0300

    регистрация фильтров, пакетные файлы для миграций

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'users',
     'rest_framework',
-    'django_filters'
+    'django_filters',
     'rest_framework_simplejwt',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -126,7 +126,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
 
 SIMPLE_JWT = {
