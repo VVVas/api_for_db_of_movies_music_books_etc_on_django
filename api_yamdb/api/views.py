47e50b7131b24129b0a8c1a95e9800367670b7a3
VVVas <v@vvvas.ru>
2023-06-21 21:00:28 +0300

    Сортировка импортов

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,17 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+from django.db import IntegrityError
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
-from rest_framework.permissions import (AllowAny, IsAuthenticated)
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.db import IntegrityError
 
 from reviews.models import Category, Genre, Review, Title
 from .filters import TitleFilter
